a8cf911f57794222ad02c50d0ed9aab6d0c02529
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-08-17 15:20:40 +0900

    added wrapper

--- uxpin.config.js
+++ uxpin.config.js
@@ -14,7 +14,7 @@ module.exports = {
             ]
         }
       ],
-      
+      wrapper: 'src/components/UXPinWrapper/UXPinWrapper.js',
       webpackConfig: './webpack.uxpin.config.js',
     },
     name: 'Evans-Library',
