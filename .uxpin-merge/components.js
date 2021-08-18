6a651334f5f9555d5e42ebae18edda4d675cf55f
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-08-18 16:01:19 +0900

    added custome branch to config.yaml

--- .uxpin-merge/components.js
+++ .uxpin-merge/components.js
@@ -3,14 +3,12 @@ import * as ReactDOM from 'react-dom';
 import Button from '../src/components/Button/Button';
 import ButtonM from '../src/components/ButtonM/ButtonM';
 import CardM from '../src/components/CardM/CardM';
-import CheckboxM from '../src/components/CheckboxM/CheckboxM';
 import Api from '../src/components/Api/Api';
 import Wrapper from '../src/components/UXPinWrapper/UXPinWrapper.js';
 export {
   Button,
   ButtonM,
   CardM,
-  CheckboxM,
   Api,
   Wrapper,
   React,
